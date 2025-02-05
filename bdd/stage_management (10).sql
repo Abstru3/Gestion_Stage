aa6a54d6eb08236afed8b0e9d9b67a55c126e4aa
KORAQI <besjan.koraqii@gmail.com>
2025-02-05 16:10:08 +0100

    maj bdd

---
+++ bdd/stage_management (10).sql
@@ -0,0 +1,359 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Mer 05 Février 2025 à 15:09
+-- Version du serveur :  5.7.11
+-- Version de PHP :  7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `stage_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `candidatures`
+--
+
+CREATE TABLE `candidatures` (
+  `id` int(11) NOT NULL,
+  `etudiant_id` int(11) DEFAULT NULL,
+  `offre_id` int(11) DEFAULT NULL,
+  `statut` enum('en_attente','acceptee','refusee') DEFAULT 'en_attente',
+  `date_candidature` datetime DEFAULT CURRENT_TIMESTAMP,
+  `cv` varchar(255) NOT NULL,
+  `lettre_motivation` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `candidatures`
+--
+
+INSERT INTO `candidatures` (`id`, `etudiant_id`, `offre_id`, `statut`, `date_candidature`, `cv`, `lettre_motivation`) VALUES
+(3, 1, 30, 'en_attente', '2025-02-05 15:05:22', '67a37022928cc.pdf', '67a3702292f3b.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `competences`
+--
+
+CREATE TABLE `competences` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `entreprises`
+--
+
+CREATE TABLE `entreprises` (
+  `id` int(11) NOT NULL,
+  `siret` varchar(14) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  `ville` varchar(100) DEFAULT NULL,
+  `code_postal` varchar(10) DEFAULT NULL,
+  `pays` varchar(100) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `site_web` varchar(255) DEFAULT NULL,
+  `lieu` varchar(255) DEFAULT NULL,
+  `tva_intracommunautaire` varchar(50) DEFAULT NULL,
+  `secteur_activite` varchar(100) DEFAULT NULL,
+  `description` text,
+  `adresse_facturation` varchar(255) DEFAULT NULL,
+  `logo` varchar(255) DEFAULT NULL,
+  `nom_contact` varchar(100) DEFAULT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('etudiant','entreprise','admin') NOT NULL DEFAULT 'entreprise'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `entreprises`
+--
+
+INSERT INTO `entreprises` (`id`, `siret`, `nom`, `adresse`, `ville`, `code_postal`, `pays`, `telephone`, `site_web`, `lieu`, `tva_intracommunautaire`, `secteur_activite`, `description`, `adresse_facturation`, `logo`, `nom_contact`, `username`, `email`, `password`, `role`) VALUES
+(2, '03215874859658', 'iutnervers', NULL, NULL, NULL, NULL, '0744813941', 'https://www.exemple.com', NULL, NULL, NULL, 'sssssssssssssssssssssssssssssssssssssss', '42 Rue Des Tailles, 58000 Nevers, France', NULL, 'Besjan Koraqi', 's', 'besjan.koraqii@gmail.com', '$2y$10$QyWJW6rO8Ir0n2TNeMszV.hGdVw307d6PCqflc.cSc6swQRpErKIO', 'entreprise');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etudiants`
+--
+
+CREATE TABLE `etudiants` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `date_naissance` date DEFAULT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  `ville` varchar(100) DEFAULT NULL,
+  `code_postal` varchar(10) DEFAULT NULL,
+  `pays` varchar(100) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `niveau_etude` varchar(50) DEFAULT NULL,
+  `filiere` varchar(100) DEFAULT NULL,
+  `cv` varchar(255) DEFAULT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('etudiant','entreprise','admin') NOT NULL DEFAULT 'etudiant',
+  `lettre_motivation` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `etudiants`
+--
+
+INSERT INTO `etudiants` (`id`, `nom`, `prenom`, `date_naissance`, `adresse`, `ville`, `code_postal`, `pays`, `telephone`, `niveau_etude`, `filiere`, `cv`, `username`, `email`, `password`, `role`, `lettre_motivation`) VALUES
+(1, 'Koraqi', 'Besjan', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'f585', 'aqii@gmail.com', '$2y$10$QUo31Cnq3k5YPDzaLjGyn.z1D9rWD5DalNBYzSibwyDMz7EiLnvYq', 'etudiant', NULL),
+(2, 'Koraqi', 'Besjan', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'admin', 'besjan.koraqii@gmail.com', '$2y$10$3tfz4UYU4p1NULVNWxjcLOwL/yJBGTzS/CEvSScqL5NZWviON62WG', 'admin', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `expediteur_id` int(11) NOT NULL,
+  `destinataire_id` int(11) NOT NULL,
+  `contenu` text NOT NULL,
+  `date_envoi` datetime DEFAULT CURRENT_TIMESTAMP,
+  `statut` enum('non_lu','lu') DEFAULT 'non_lu'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `etudiant_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `date_notification` datetime DEFAULT CURRENT_TIMESTAMP,
+  `statut` enum('non_lu','lu') DEFAULT 'non_lu'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offres_competences`
+--
+
+CREATE TABLE `offres_competences` (
+  `offre_id` int(11) NOT NULL,
+  `competence_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offres_stages`
+--
+
+CREATE TABLE `offres_stages` (
+  `id` int(11) NOT NULL,
+  `entreprise_id` int(11) DEFAULT NULL,
+  `titre` varchar(100) NOT NULL,
+  `description` text,
+  `date_debut` date DEFAULT NULL,
+  `date_fin` date DEFAULT NULL,
+  `lieu` varchar(255) DEFAULT NULL,
+  `mode_stage` enum('distanciel','présentiel') NOT NULL,
+  `logo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `offres_stages`
+--
+
+INSERT INTO `offres_stages` (`id`, `entreprise_id`, `titre`, `description`, `date_debut`, `date_fin`, `lieu`, `mode_stage`, `logo`) VALUES
+(4, NULL, 'Stage numéro 1 en informatique', 'ss', '2025-01-30', '2025-02-27', NULL, 'distanciel', NULL),
+(30, 2, 'Stage numéro 1 en informatique', 'juuuu', '2025-02-05', '2025-02-28', 'nevers , 45 rue des boloss', 'présentiel', 'uploads/logos/202303-7-1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stages`
+--
+
+CREATE TABLE `stages` (
+  `id` int(11) NOT NULL,
+  `etudiant_id` int(11) NOT NULL,
+  `offre_id` int(11) NOT NULL,
+  `date_debut` date NOT NULL,
+  `date_fin` date NOT NULL,
+  `statut` enum('en_cours','terminé','annulé') DEFAULT 'en_cours'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `candidatures`
+--
+ALTER TABLE `candidatures`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `etudiant_id` (`etudiant_id`,`offre_id`),
+  ADD KEY `offre_id` (`offre_id`);
+
+--
+-- Index pour la table `competences`
+--
+ALTER TABLE `competences`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `entreprises`
+--
+ALTER TABLE `entreprises`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `siret` (`siret`);
+
+--
+-- Index pour la table `etudiants`
+--
+ALTER TABLE `etudiants`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `expediteur_id` (`expediteur_id`),
+  ADD KEY `destinataire_id` (`destinataire_id`);
+
+--
+-- Index pour la table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `notifications_fk` (`etudiant_id`);
+
+--
+-- Index pour la table `offres_competences`
+--
+ALTER TABLE `offres_competences`
+  ADD PRIMARY KEY (`offre_id`,`competence_id`),
+  ADD KEY `competence_id` (`competence_id`);
+
+--
+-- Index pour la table `offres_stages`
+--
+ALTER TABLE `offres_stages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `entreprise_id` (`entreprise_id`);
+
+--
+-- Index pour la table `stages`
+--
+ALTER TABLE `stages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `etudiant_id` (`etudiant_id`),
+  ADD KEY `offre_id` (`offre_id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `candidatures`
+--
+ALTER TABLE `candidatures`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT pour la table `competences`
+--
+ALTER TABLE `competences`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `entreprises`
+--
+ALTER TABLE `entreprises`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `etudiants`
+--
+ALTER TABLE `etudiants`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `offres_stages`
+--
+ALTER TABLE `offres_stages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+--
+-- AUTO_INCREMENT pour la table `stages`
+--
+ALTER TABLE `stages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `candidatures`
+--
+ALTER TABLE `candidatures`
+  ADD CONSTRAINT `candidatures_ibfk_1` FOREIGN KEY (`etudiant_id`) REFERENCES `etudiants` (`id`),
+  ADD CONSTRAINT `candidatures_ibfk_2` FOREIGN KEY (`offre_id`) REFERENCES `offres_stages` (`id`);
+
+--
+-- Contraintes pour la table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_fk` FOREIGN KEY (`etudiant_id`) REFERENCES `etudiants` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `offres_competences`
+--
+ALTER TABLE `offres_competences`
+  ADD CONSTRAINT `offres_competences_ibfk_1` FOREIGN KEY (`offre_id`) REFERENCES `offres_stages` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `offres_competences_ibfk_2` FOREIGN KEY (`competence_id`) REFERENCES `competences` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `offres_stages`
+--
+ALTER TABLE `offres_stages`
+  ADD CONSTRAINT `offres_stages_ibfk_1` FOREIGN KEY (`entreprise_id`) REFERENCES `entreprises` (`id`);
+
+--
+-- Contraintes pour la table `stages`
+--
+ALTER TABLE `stages`
+  ADD CONSTRAINT `stages_ibfk_1` FOREIGN KEY (`etudiant_id`) REFERENCES `etudiants` (`id`),
+  ADD CONSTRAINT `stages_ibfk_2` FOREIGN KEY (`offre_id`) REFERENCES `offres_stages` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
